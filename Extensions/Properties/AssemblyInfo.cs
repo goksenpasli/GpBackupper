da856aab0ec013bdde192926d1d7f30db61ac8c9
goksenpasli <gpasli@gmail.com>
2021-07-20 08:40:05 +0300

    Merge branch 'master' of https://github.com/goksenpasli/GpBackupper

--- Extensions/Properties/AssemblyInfo.cs
+++ Extensions/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Extensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GpExtensions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Goksen")]
 [assembly: AssemblyProduct("Extensions")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyTrademark("Goksen")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
